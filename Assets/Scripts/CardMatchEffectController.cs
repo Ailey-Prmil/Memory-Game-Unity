fe67084c5c1758cc3670819ff9c1adc0724c07ef
Uyen324 <uyen030204@gmail.com>
2024-11-10 14:18:23 +0700

    Dissolve effect

---
+++ Assets/Scripts/CardMatchEffectController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CardMatchEffectController : MonoBehaviour
+{
+    public ParticleSystem sparkleEffectPrefab; // Prefab của Particle System
+
+    // Phương thức tạo hiệu ứng riêng biệt tại mỗi vị trí
+    public void PlayEffectAtPosition(Vector3 position)
+    {
+        // Tạo bản sao của Particle System tại vị trí chỉ định
+        ParticleSystem sparkle = Instantiate(sparkleEffectPrefab, position, Quaternion.identity);
+        sparkle.Play();
+
+        // Hủy bản sao sau khi hiệu ứng kết thúc
+        Destroy(sparkle.gameObject, sparkle.main.duration);
+    }
+}
